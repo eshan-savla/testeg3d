5699c49c79b96d1d0bfe2cbe313bcc3c1a0614bf
chl-es <eshan.savla@ipa.fraunhofer.de>
2023-03-24 22:21:09 +0100

    script to create ground truth

---
+++ scripts/ground_truth_creator.py
@@ -0,0 +1,48 @@
+import numpy as np
+import open3d as o3d
+
+
+def main():
+    ground_truth = o3d.geometry.PointCloud()
+    y_dimension, x_dimension, z_dimension = 200, 100, 50
+    points = np.zeros((y_dimension, x_dimension + z_dimension, 3), dtype=np.float64)
+    start_val = 0.01
+    step_size = 0.001
+    z_const_val = start_val
+    x_const_val = start_val + ((x_dimension / 2) * step_size)
+    z_top_val = start_val + (z_dimension * step_size)
+    y_count = 0
+
+    for line in points:
+        y = start_val + (y_count * step_size)
+        x_count, z_count = 0, 0
+        for point in line:
+            if x_count < x_dimension / 2:
+                x = start_val + (x_count * step_size)
+                z = z_const_val
+                x_count += 1
+            elif z_count >= z_dimension:
+                x = start_val + (x_count * step_size)
+                z = z_top_val
+                x_count += 1
+            else:
+                x = x_const_val
+                z = start_val + (z_count * step_size)
+                z_count += 1
+            point[0] = x
+            point[1] = y
+            point[2] = z
+        y_count += 1
+
+    np.save("/home/chl-es/TestEG3D/src/testeg3d/data/ground_truth", points)
+    print("Wrote .npy file to ../data/ground_truth.npy")
+    ground_truth.points = o3d.utility.Vector3dVector(np.concatenate(points, axis=0))
+    print("Created point cloud with: ")
+    print(ground_truth)
+    o3d.io.write_point_cloud("/home/chl-es/TestEG3D/src/testeg3d/data/ground_truth.pcd", ground_truth)
+    print(".pcd file created at ../data/ground_truth.pcd")
+    return None
+
+
+if __name__ == "__main__":
+    main()
